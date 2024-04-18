7639af0866f2df72796dde36cab81a4cbb7a32fa
ysfclskn <ysfclskn94@hotmail.com>
2024-04-18 19:44:44 +0300

    UP-001 update project

--- src/test/java/testautomation/runners/CucumberRunner.java
+++ src/test/java/testautomation/runners/CucumberRunner.java
@@ -5,13 +5,15 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+
 @CucumberOptions(
-        plugin = {"html:target/default-html-reports","io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm"},
+        plugin = {"html:target/default-html-reports", "io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm"},
         glue = "testautomation/step_def",
         features = "src/test/resources/features",
         dryRun = false,
         tags = "@smoke"
 )
+
 public class CucumberRunner {
 
 }
